e2b176e7f8fe31f7fef93d064b952685003a1961
Xu Chen <xuc323@lehigh.edu>
2021-06-13 22:53:20 -0400

    welcome messages

--- main.js
+++ main.js
@@ -11,8 +11,18 @@ client.on("ready", function () {
     console.log("Bot is online!");
 });
 
+// create an event listener for new guild members
+client.on("guildMemberAdd", function (mem) {
+    // assign new members as "member" role
+    const role = mem.guild.roles.cache.find(role => role.name === "member");
+    mem.roles.add(role);
+    // send the message to "welcome" channel
+    mem.guild.channels.cache.find(ch => ch.name === "welcome").send(`Welcome ${mem}, your are now the member of this server!`);
+});
+
 // create an event listener for messages
 client.on("message", function (message) {
+    // bot will not respond to message without prefix "-" or message from itself
     if (!message.content.startsWith(prefix) || message.author.bot) {
         return;
     }
