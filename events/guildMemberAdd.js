5ff9e16ba7c863b0663d306500e606df92b16d17
Xu Chen <xuc323@lehigh.edu>
2021-06-15 21:39:33 -0400

    No longer pass in prefix

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: "guildMemberAdd",
-    execute(mem) {
+    execute(member) {
         // assign new members as "member" role
-        const role = mem.guild.roles.cache.get("853821008682811392");
-        mem.roles.add(role);
+        const role = member.guild.roles.cache.get("853821008682811392");
+        member.roles.add(role);
         // send the message to "welcome" channel
-        mem.guild.channels.cache.get("853793532627648512").send(`Welcome ${mem}, now you have the \`${role.name}\` role of this server!`);
+        member.guild.channels.cache.get("853793532627648512").send(`Welcome ${member}, now you have the \`${role.name}\` role of this server!`);
     }
 }
