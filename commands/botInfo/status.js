d593154941fa9664ff9c7870b12dcfbb36cb3f78
Xu Chen <xuc323@lehigh.edu>
2021-07-02 13:47:31 -0400

    Updated usage for status

--- commands/botInfo/status.js
+++ commands/botInfo/status.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "status",
     description: "Set the status of the bot",
     args: true,
+    usage: "[status]",
     execute(message, args, client) {
         client.user.setPresence({
             activity: {
