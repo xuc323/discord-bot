77d1654c382c622240f8564c180d98b8783404ef
Xu Chen <xuc323@lehigh.edu>
2021-07-10 22:05:29 -0400

    Play now uses music bot library

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,28 +1,16 @@
 module.exports = {
     name: "play",
-    description: "Play the music from Youtube link",
+    description: "Play the music by name or url",
     aliases: ["p"],
     args: true,
-    usage: "[url]",
-    execute(message, args, client) {
-        const voiceChannel = message.member.voice.channel;
-        if (!voiceChannel) {
-            return message.reply("Seems that you are not in one of the voice channels. Join one and try again!");
+    usage: "[song name or url]",
+    async execute(message, args, client) {
+        const song = await client.player.play(message, {
+            search: args.join(" "),
+            requestedBy: message.author.tag
+        });
+        if (song) {
+            console.log(`Playing ${song.name}`);
         }
-
-        const permissions = voiceChannel.permissionsFor(message.client.user);
-        if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-            return message.reply("I need the permissions to join and speak in your current voice channel.");
-        }
-        
-        voiceChannel.join().then((connection => {
-            ytdl.getInfo(args).then((info) => {
-                message.channel.send(`Song found: ${info.videoDetails.title} at ${info.videoDetails.video_url}`);
-            });
-            const stream = ytdl(args, { filter: "audioonly" });
-            const dispatcher = connection.play(stream);
-            dispatcher.on("finish", () => voiceChannel.leave());
-        }));
-
     }
 }
