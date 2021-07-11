f9841333c03f9f31eca8d45a17746e7cda28253f
Xu Chen <xuc323@lehigh.edu>
2021-07-11 17:16:05 -0400

    Console.log error

--- music-events/error.js
+++ music-events/error.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "error",
     execute(error, message) {
-        console.log(error);
+        console.log("ERROR: ", error);
         // switch (error) {
         //     case value:
 
