b2399c490e843b57d4da6532ebd35b6faabdf705
Xu Chen <xuc323@lehigh.edu>
2021-12-16 12:25:23 -0500

    Fixed help function so bot can reply

--- commands/botInfo/help.js
+++ commands/botInfo/help.js
@@ -4,19 +4,20 @@ module.exports = {
     name: "help",
     description: "List all commands or info about a specific command.",
     usage: "[command name]",
-    execute(message, args) {
+    aliases: ["h"],
+    execute(message, args, client) {
         const data = [];
         const { commands } = message.client;
         if (!args.length) {
             data.push("Supported commands:\n");
             data.push(commands.map(command => `**${command.name}** ${command.description}`).join("\n"));
             data.push(`\nYou can send\`${prefix}help [command name]\` to get info on a specific command.`);
-            message.channel.send(data, { split: true });
+            message.channel.send(data.join("\n"));
         } else {
             const name = args[0].toLowerCase();
             const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
             if (!command) {
-                return message.reply("That's not a valid command!");
+                return message.channel.send(`\`${args[0]}\` is not a valid command. Type \`${prefix}help\` for more information.`);
             }
             data.push(`**Name:** ${command.name}`);
             if (command.aliases) {
@@ -30,7 +31,7 @@ module.exports = {
             } else {
                 data.push(`**Usage:** \`${prefix}${command.name}\``);
             }
-            message.channel.send(data, { split: true });
+            message.channel.send(data.join("\n"));
         }
     }
 }
