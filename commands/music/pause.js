b557e41106c23479b74c605cb36ae841c84a5b0e
Xu Chen <xuc323@lehigh.edu>
2021-12-25 17:59:55 -0500

    next, pause, resume now support discordjs v13

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -2,10 +2,8 @@ module.exports = {
     name: "pause",
     description: "Pause the current song.",
     args: false,
-    execute(message, args, client) {
-        const song = client.player.pause(message);
-        if (song) {
-            message.channel.send(`**${song.name}** was paused!`);
-        }
+    execute(message, args, client, guildQueue) {
+        guildQueue.setPaused(true);
+        message.channel.send("The queue is now paused!");
     }
 }
