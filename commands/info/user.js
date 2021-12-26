c80f85e142a7b8bb4108bf1fc6e3acf8c9d15b0a
Xu Chen <xuc323@lehigh.edu>
2021-12-25 19:39:54 -0500

    Sending embed messages for server and user info

--- commands/info/user.js
+++ commands/info/user.js
@@ -1,8 +1,29 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     name: "user",
     description: "Display user info.",
     args: false,
     execute(message, args, client) {
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+        const { createdAt, id, username, tag } = message.author;
+        const icon = message.author.avatarURL();
+        const emb = new MessageEmbed()
+            .setTitle(`User info for \`${username}\``)
+            .setThumbnail(icon)
+            .addFields(
+                [{
+                    name: "ID ",
+                    value: id
+                },
+                {
+                    name: "Tag ",
+                    value: tag
+                },
+                {
+                    name: "Created at ",
+                    value: createdAt.toString()
+                }]
+            );
+        message.channel.send({ embeds: [emb] });
     }
 }
