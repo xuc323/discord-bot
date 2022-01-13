f08e8879466b8941220a792d75bb6e319c05b279
Xu Chen <xuc323@lehigh.edu>
2022-01-13 18:19:29 -0500

    Added shuffle, playlistAdd event, playlist can now be added

---
+++ music_events/playlistAdd.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "playlistAdd",
+    execute(queue, playlist) {
+        // retrive the initial message channel from the queue
+        const channel = queue.data.msgChannel;
+        channel.send(`**${playlist.name}** has been added to the queue.\n${playlist.url}`);
+        console.log(`[${queue.guild.name} | ${queue.guild.id} | ${playlist.songs[0].requestedBy}]: ${playlist} ${playlist.url}`);
+    }
+}
