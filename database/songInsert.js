2b1c1197a73b4fddc7afd56712cb9fac272b1f23
Xu Chen <xuc323@lehigh.edu>
2022-01-19 13:16:30 -0500

    Now able to insert without conflicts

--- database/songInsert.js
+++ database/songInsert.js
@@ -4,21 +4,41 @@ module.exports = {
         try {
             uid = (await guildInsert(client, guild.uid, guild.name)).rows[0].uid;
             sid = (await songInsert(client, song.name, song.url)).rows[0].sid;
+            await requestInsert(client, user, uid, sid);
         } catch (err) {
             console.log(err.stack);
         }
-        requestInsert(client, user, uid, sid);
     }
 }
 
 function guildInsert(client, gid, name) {
-    const statement = `INSERT INTO guilds(guildId, name) VALUES($1, $2) ON CONFLICT DO NOTHING RETURNING uid;`;
+    const statement = `
+        WITH e AS(
+            INSERT INTO guilds (guildId, name) 
+                VALUES ($1, $2)
+            ON CONFLICT (guildId) DO NOTHING
+            RETURNING uid
+        )
+        SELECT * FROM e
+        UNION
+            SELECT uid FROM guilds WHERE guildId=$1;
+        `;
     const values = [gid, name];
     return client.postgres.query(statement, values);
 }
 
 function songInsert(client, name, url) {
-    const statement = `INSERT INTO songs(name, url) VALUES($1, $2) ON CONFLICT DO NOTHING RETURNING sid;`;
+    const statement = `
+        WITH e AS(
+            INSERT INTO songs (name, url) 
+                VALUES ($1, $2)
+            ON CONFLICT (url) DO NOTHING
+            RETURNING sid
+        )
+        SELECT * FROM e
+        UNION
+            SELECT sid FROM songs WHERE url=$2;
+        `;
     const values = [name, url];
     return client.postgres.query(statement, values);
 }
@@ -26,11 +46,5 @@ function songInsert(client, name, url) {
 function requestInsert(client, userId, uid, sid) {
     const statement = `INSERT INTO requests(uid, sid, userId) VALUES($1, $2, $3);`;
     const values = [uid, sid, userId];
-    client.postgres.query(statement, values, (err, res) => {
-        if (err) {
-            console.log(err.stack);
-        } else {
-            // console.log(res);
-        }
-    });
+    return client.postgres.query(statement, values);
 }
