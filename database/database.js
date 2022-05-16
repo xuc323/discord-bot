b5523d115e7df83ad0bcefc2dc736c9745c3acfa
xuc323 <xuc323@lehigh.edu>
2022-05-16 17:26:35 -0400

    bring back database

--- database/database.js
+++ database/database.js
@@ -1,10 +1,12 @@
 const { Client } = require('pg');
+const { createTables, guildInsert, songInsert, userInsert, requestInsert } = require('./util');
 
+// database class
 class Database {
-    constructor(config) {
+    constructor(url) {
         // create an instance of the database client
         this.client = new Client({
-            connectionString: config,
+            connectionString: url,
             ssl: {
                 rejectUnauthorized: false
             }
@@ -17,40 +19,7 @@ class Database {
             } else {
                 console.log("Postgres database connected!");
                 // after connection, create table if not exists
-                // query statement to create relational table if not exists
-                const statement = `
-                    CREATE TABLE IF NOT EXISTS guilds(
-                        gid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-                        id BIGINT UNIQUE NOT NULL,
-                        name TEXT NOT NULL
-                    );
-                    CREATE TABLE IF NOT EXISTS songs(
-                        sid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-                        name TEXT NOT NULL,
-                        url TEXT UNIQUE NOT NULL,
-                        author TEXT NOT NULL
-                    );
-                    CREATE TABLE IF NOT EXISTS users(
-                        uid uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-                        username TEXT NOT NULL,
-                        discriminator VARCHAR(10) NOT NULL,
-                        id BIGINT UNIQUE NOT NULL
-                    );
-                    CREATE TABLE IF NOT EXISTS requests(
-                        gid uuid NOT NULL REFERENCES guilds (gid),
-                        sid uuid NOT NULL REFERENCES songs (sid),
-                        uid uuid NOT NULL REFERENCES users (uid),
-                        date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                    );
-                `;
-
-                this.client.query(statement, (err, res) => {
-                    if (err) {
-                        console.error("Error creating tables: ", err.stack);
-                    } else {
-                        console.log("Tables created!");
-                    }
-                });
+                createTables(this.client);
             }
         });
 
@@ -65,62 +34,34 @@ class Database {
         });
     }
 
-    guildInsert(gid, name) {
-        // prepare query statement
-        const statement = `
-            INSERT INTO guilds(id, name)
-                VALUES($1, $2)
-            ON CONFLICT (id) 
-            DO 
-                UPDATE SET name=$2
-            RETURNING gid;
-        `;
-
-        // insert guild into database
-        return this.client.query(statement, [gid, name]);
+    /**
+     * When a song is requested, insert guild, song
+     * and user into into database. Each insertion returns
+     * a promise that resolves into unique id, and it's necessary
+     * for the request table.
+     * @param {Object} guild 
+     * @param {BigInt} guild.id the guild id from discord
+     * @param {String} guild.name the guild name from discord
+     * @param {Object} song
+     * @param {String} song.name the song name from youtube
+     * @param {String} song.url the song url from youtube
+     * @param {String} song.author the song author from youtube
+     * @param {Object} user
+     * @param {String} user.username the user name from discord
+     * @param {String} user.discriminator the user discriminator from discord
+     * @param {BigInt} user.id the user id from discord
+     */
+    playSongInsert(guild, song, user) {
+        const gid_promise = guildInsert(this.client, guild.gid, guild.name);
+        const sid_promise = songInsert(this.client, song.name, song.url, song.author);
+        const uid_promise = userInsert(this.client, user.username, user.discriminator, user.id);
+        Promise.all([gid_promise, sid_promise, uid_promise]).then((values) => {
+            const gid = values[0].rows[0].gid;
+            const sid = values[1].rows[0].sid;
+            const uid = values[2].rows[0].uid;
+            requestInsert(this.client, gid, sid, uid);
+        }).catch(err => console.error(err));
     }
-
-    songInsert(name, url, author) {
-        // prepare query statement
-        const statement = `
-            INSERT INTO songs(name, url, author)
-                VALUES($1, $2, $3)
-            ON CONFLICT (url) 
-            DO 
-                UPDATE SET name=$1, author=$3
-            RETURNING sid;
-        `;
-
-        // insert song into database
-        return this.client.query(statement, [name, url, author]);
-    }
-
-    userInsert(username, discriminator, id) {
-        // prepare query statement
-        const statement = `
-            INSERT INTO users(username, discriminator, id)
-                VALUES($1, $2, $3)
-            ON CONFLICT (id) 
-            DO 
-                UPDATE SET username=$1, discriminator=$2
-            RETURNING uid;
-        `;
-
-        // insert user into database
-        return this.client.query(statement, [username, discriminator, id]);
-    }
-
-    requestInsert(gid, sid, uid) {
-        // prepare query statement
-        const statement = `
-            INSERT INTO requests(gid, sid, uid)
-                VALUES($1, $2, $3);
-        `;
-
-        // insert request into database
-        return this.client.query(statement, [gid, sid, uid]);
-    }
-
 }
 
 module.exports = Database;
