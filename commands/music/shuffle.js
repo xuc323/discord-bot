f68eedc457b3c52cd903f252f320dfafcfb238ea
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:06 -0500

    Added shuffle, playlistAdd event, playlist can now be added

---
+++ commands/music/shuffle.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "shuffle",
+    description: "Shuffle the queue.",
+    aliases: ["shuf"],
+    args: false,
+    execute(message, args, client, guildQueue) {
+        if (guildQueue) {
+            // the queue exists
+            // retrive the initial message channel from the queue
+            const channel = guildQueue.data.msgChannel;
+            if (message.channel === channel) {
+                // the message is from the same channel the queue was created
+                guildQueue.shuffle();
+                message.channel.send("Queue is now shuffled!");
+            } else {
+                // the message is not from the same channel the queue was created
+                message.channel.send(`The queue was created in another text channel.\nPlease head to channel ${channel} for music commands.`);
+            }
+        } else {
+            // the queue doesn't exist
+            message.channel.send("ERROR: Queue is empty, can't perform \`shuffle\`.");
+        }
+    }
+}
--- music_events/error.js
+++ music_events/error.js
@@ -3,7 +3,7 @@ module.exports = {
     execute(error, queue) {
         // retrive the initial message channel from the queue
         const channel = queue.data.msgChannel;
-        channel.send(`ERROR: ${error}`);
+        channel.send(`ERROR: \`${error.message}\`.`);
         console.log(`[${queue.guild.name} | ${queue.guild.id}]: Error: ${error}`);
     }
 }
