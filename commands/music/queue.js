a2739ae022fdd832f4ec757df1c65a3804f9f24b
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:06 -0500

    Add more elements in the queue embeded message

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -11,12 +11,13 @@ module.exports = {
             for (let i = 0; i < guildQueue.songs.length; i++) {
                 const song = guildQueue.songs[i];
                 if (i === 0) {
-                    emb.setThumbnail(song.thumbnail).addField("Now Playing", `${song.name}\n${song.author}\n${song.url}\nRequested by: ${song.requestedBy}`);
+                    emb.setThumbnail(song.thumbnail)
+                        .addField("Now Playing", `**Name:** ${song.name}\n**Author:** ${song.author}\n**Link:** ${song.url}\n**Requested by:** ${song.requestedBy}`);
                 } else {
-                    emb.addField(`#${i + 1}`, `${song.name}\n${song.author}\n${song.url}\nRequested by: ${song.requestedBy}`);
+                    emb.addField(`#${i + 1}`, `**Name:** ${song.name}\n**Author:** ${song.author}\n**Link:** ${song.url}\n**Requested by:** ${song.requestedBy}`);
                 }
             }
-            emb.setFooter(`Count: ${guildQueue.songs.length}`);
+            emb.setFooter(`Music count: ${guildQueue.songs.length}`);
         } else {
             emb.setFooter("Queue is empty.")
         }
