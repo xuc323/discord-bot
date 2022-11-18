a6fa57bb6bec77fb935b80991a8ae42172ee0b3f
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:03 -0500

    Deleted alias for skip

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: "skip",
     description: "Skip the current song.",
-    aliases: ["s"],
     args: false,
     execute(message, args, client) {
         const song = client.player.skip(message);
