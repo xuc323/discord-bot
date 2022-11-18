27c3a6f08c29d232da5961181b24b9c037e536e2
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:02 -0500

    Now able to update the status of the bot

---
+++ commands/bot/status.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "status",
+    description: "Set the status of the bot",
+    args: true,
+    execute(message, args, client) {
+        client.user.setPresence({
+            activity: {
+                name: args.join(" "),
+                type: 0
+            }
+        });
+    }
+}
