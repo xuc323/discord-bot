e2d1212182d3e78b9b4600e151604fcaee638b9b
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:04 -0500

    Removed async for pause

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -2,8 +2,8 @@ module.exports = {
     name: "pause",
     description: "Pause the current song.",
     args: false,
-    async execute(message, args, client) {
-        const song = await client.player.pause(message);
+    execute(message, args, client) {
+        const song = client.player.pause(message);
         if (song) {
             message.channel.send(`**${song.name}** was paused!`);
         }
