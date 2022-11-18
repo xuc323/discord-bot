f170f0d51b1080243da54e316c2daada191220d5
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:06 -0500

    Music commands now only accepted at the initial channel

--- commands/music/setVolume.js
+++ commands/music/setVolume.js
@@ -6,14 +6,24 @@ module.exports = {
     usage: "[volume number]",
     execute(message, args, client, guildQueue) {
         if (guildQueue) {
-            const vol = parseInt(args[0]);
-            const status = guildQueue.setVolume(vol);
-            if (status) {
-                message.channel.send(`Volume set to ${vol}%.`);
+            // the queue exists
+            // retrive the initial message channel from the queue
+            const channel = guildQueue.data.msgChannel;
+            if (message.channel === channel) {
+                // the message is from the same channel the queue was created
+                const vol = parseInt(args[0]);
+                const status = guildQueue.setVolume(vol);
+                if (status) {
+                    message.channel.send(`Volume set to ${vol}%.`);
+                } else {
+                    message.channel.send("ERROR: Failed to set volume.");
+                }
             } else {
-                message.channel.send("ERROR: Failed to set volume.");
+                // the message is not from the same channel the queue was created
+                message.channel.send(`The queue was created in another text channel.\nPlease head to channel ${channel} for music commands.`);
             }
         } else {
+            // the queue doesn't exist
             message.channel.send("ERROR: Queue is empty, can't perform \`volume\`.");
         }
     }
--- music_events/songAdd.js
+++ music_events/songAdd.js
@@ -4,6 +4,6 @@ module.exports = {
         // retrive the initial message channel from the queue
         const channel = queue.data.msgChannel;
         channel.send(`**${song.name}** has been added to the queue.\n${song.url}`);
-        console.log(`[${queue.guild.name} | ${queue.guild.id} | ${song.requestedBy}]: Song ${song} was added to the queue.`);
+        console.log(`[${queue.guild.name} | ${queue.guild.id} | ${song.requestedBy}]: ${song} ${song.url}`);
     }
 }
