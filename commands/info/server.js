22e8f5a262dd00ca510803b0675ea6408e87ba1f
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:01 -0500

    Adding descriptions and help

--- commands/info/server.js
+++ commands/info/server.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "server",
-    description: "Server info",
+    description: "Display server info.",
+    args: false,
     execute(message, args) {
         message.reply(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\nServer created on: ${message.guild.createdAt}\nServer region: ${message.guild.region}`);
     }
