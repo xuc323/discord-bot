8c163e1a2a471645205b96dba50457dec535a252
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:05 -0500

    Play function is now updated to support discordjs v13

--- commands/music/play.js
+++ commands/music/play.js
@@ -4,14 +4,18 @@ module.exports = {
     aliases: ["p"],
     args: true,
     usage: "[song name or url]",
-    execute(message, args, client) {
-        client.player.play(message, {
-            search: args.join(" "),
-            requestedBy: message.author.tag
-        }).then((song) => {
-            if (song) {
-                console.log(`Playing ${song.name}`);
-            }
+    execute(message, args, client, guildQueue) {
+        let queue = client.player.createQueue(message.guild.id);
+        console.log(queue);
+        queue.join(message.member.voice.channel).then(() => {
+            queue.play(args.join(" "), { requestedBy: message.author.tag }).then((song) => {
+                console.log(song);
+            }).catch((err) => {
+                console.log(err);
+                if (!guildQueue) {
+                    queue.stop();
+                }
+            });
         });
     }
 }
