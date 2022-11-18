0acaea11af794e75739b9e36f8783818fab5191e
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:02 -0500

    Moved files

--- commands/botInfo/help.js
+++ commands/botInfo/help.js
@@ -9,17 +9,17 @@ module.exports = {
         const data = [];
         const { commands } = message.client;
         if (!args.length) {
-            data.push("\nHere's a list of all commands: ");
+            data.push("Here's a list of all commands: ");
             data.push(commands.map(command => command.name).join(", "));
             data.push(`\nYou can send\`${prefix}help [command name]\` to get info on a specific command.`);
-            return message.reply(data, { split: true });
+            return message.channel.send(data, { split: true });
         } else {
             const name = args[0].toLowerCase();
             const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
             if (!command) {
                 return message.reply("That's not a valid command!");
             }
-            data.push(`\n**Name:** ${command.name}`);
+            data.push(`**Name:** ${command.name}`);
             if (command.aliases) {
                 data.push(`**Aliases:** ${command.aliases.join(", ")}`);
             }
@@ -31,7 +31,7 @@ module.exports = {
             } else {
                 data.push(`**Usage:** \`${prefix}${command.name}\``);
             }
-            message.reply(data, { split: true });
+            message.channel.send(data, { split: true });
         }
     }
 }
