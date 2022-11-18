369a8f95a43c8fc98e709f14bb3ede896bdb8738
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:03 -0500

    Included a status list

--- commands/botInfo/status.js
+++ commands/botInfo/status.js
@@ -2,12 +2,39 @@ module.exports = {
     name: "status",
     description: "Set the status of the bot",
     args: true,
-    usage: "[status]",
+    usage: "[status number] [status]",
     execute(message, args, client) {
+        let type;
+        switch (args[0]) {
+            case "1":
+                type = "PLAYING";
+                break;
+            case "2":
+                type = "STREAMING";
+                break;
+            case "3":
+                type = "LISTENING";
+                break;
+            case "4":
+                type = "WATCHING";
+                break;
+            case "5":
+                type = "COMPETING";
+                break;
+            default:
+                break;
+        }
+
+        if (type === undefined) {
+            type = "PLAYING";
+        } else {
+            args.shift();
+        }
+
         client.user.setPresence({
             activity: {
                 name: args.join(" "),
-                type: "LISTENING"
+                type: type
             }
         });
     }
