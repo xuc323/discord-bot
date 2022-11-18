a50178d322947f373f44970f8f281c958b998ca3
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:03 -0500

    Added queue function

---
+++ commands/music/queue.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "queue",
+    description: "Display the server queue",
+    args: false,
+    execute(message, args, client) {
+        const queue = client.player.getQueue(message);
+        const emb = new MessageEmbed().setTitle("Queue: ");
+        if (queue) {
+            for (let i = 0; i < queue.songs.length; i++) {
+                const song = queue.songs[i];
+                if (i === 0) {
+                    emb.addField("Now Playing", `${song.name}\n${song.author}\nRequested by: ${song.requestedBy}`);
+                } else {
+                    emb.addField(`#${i + 1}`, `${song.name}\n${song.author}\nRequested by: ${song.requestedBy}`);
+                }
+            }
+            emb.setFooter(`Count: ${queue.songs.length}`);
+        } else {
+            emb.setFooter("Queue is empty.")
+        }
+        message.channel.send(emb);
+    }
+}
