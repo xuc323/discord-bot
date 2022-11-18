e76e013d45145919af64542e25ed7e8ef74b197d
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:05 -0500

    Queue function is now supporting discordjs v13

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -5,22 +5,21 @@ module.exports = {
     description: "Display the server queue",
     aliases: ["q"],
     args: false,
-    execute(message, args, client) {
-        const queue = client.player.getQueue(message);
+    execute(message, args, client, guildQueue) {
         const emb = new MessageEmbed().setTitle("Queue");
-        if (queue) {
-            for (let i = 0; i < queue.songs.length; i++) {
-                const song = queue.songs[i];
+        if (guildQueue) {
+            for (let i = 0; i < guildQueue.songs.length; i++) {
+                const song = guildQueue.songs[i];
                 if (i === 0) {
                     emb.addField("Now Playing", `${song.name}\n${song.author}\nRequested by: ${song.requestedBy}`);
                 } else {
                     emb.addField(`#${i + 1}`, `${song.name}\n${song.author}\nRequested by: ${song.requestedBy}`);
                 }
             }
-            emb.setFooter(`Count: ${queue.songs.length}`);
+            emb.setFooter(`Count: ${guildQueue.songs.length}`);
         } else {
             emb.setFooter("Queue is empty.")
         }
-        message.channel.send(emb);
+        message.channel.send({ embeds: [emb] });
     }
 }
