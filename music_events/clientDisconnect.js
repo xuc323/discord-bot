d5671fa656ab2f56f669c20a75442ccfc38ee4c2
xuc323 <79107215+xuc323@users.noreply.github.com>
2022-11-18 10:39:06 -0500

    Added more log info

--- music_events/clientDisconnect.js
+++ music_events/clientDisconnect.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "clientDisconnect",
     execute(queue) {
-        console.log(`I was kicked from the Voice Channel, queue ended.`);
+        console.log(`[${queue.guild.name} | ${queue.guild.id}]: I was kicked from the Voice Channel, queue ended.`);
     }
 }
