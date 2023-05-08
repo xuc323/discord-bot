0c1ff3da322c143f1e1502eb4d5a40343ff7ffda
Xu Chen <79107215+xuc323@users.noreply.github.com>
2023-05-07 23:03:55 -0400

    Updated packages (#78)

--- music/music.ts
+++ music/music.ts
@@ -1,70 +1,70 @@
-import { Client, Guild } from "discord.js";
-import ytdl from "ytdl-core";
-import ytsr from "ytsr";
-import { MyClient } from "../type";
+// import { Client, Guild } from "discord.js";
+// import ytdl from "ytdl-core";
+// import ytsr from "ytsr";
+// import { MyClient } from "../type";
 
-enum repeatMode {
-  None,
-  Song,
-  Queue,
-}
+// enum repeatMode {
+//   None,
+//   Song,
+//   Queue,
+// }
 
-interface song {
-  name: string;
-  duration: string;
-}
+// interface song {
+//   name: string;
+//   duration: string;
+// }
 
-interface musicQueue {
-  guild: Guild;
-  queue: song[];
-}
+// interface musicQueue {
+//   guild: Guild;
+//   queue: song[];
+// }
 
-interface playerOptions {
-  quitOnEnd: boolean;
-  quitOnEmpty: boolean;
-}
+// interface playerOptions {
+//   quitOnEnd: boolean;
+//   quitOnEmpty: boolean;
+// }
 
-class musicPlayer {
-  private client: MyClient;
-  private queue: musicQueue[];
-  private repeatMode: repeatMode;
-  private options: playerOptions;
+// class musicPlayer {
+//   private client: MyClient;
+//   private queue: musicQueue[];
+//   private repeatMode: repeatMode;
+//   private options: playerOptions;
 
-  public constructor(
-    client: Client,
-    { quitOnEmpty = false, quitOnEnd = false }: playerOptions
-  ) {
-    this.client = client;
-    this.queue = [];
-    this.repeatMode = repeatMode.None;
-    this.options = { quitOnEmpty, quitOnEnd };
-  }
+//   public constructor(
+//     client: Client,
+//     { quitOnEmpty = false, quitOnEnd = false }: playerOptions
+//   ) {
+//     this.client = client;
+//     this.queue = [];
+//     this.repeatMode = repeatMode.None;
+//     this.options = { quitOnEmpty, quitOnEnd };
+//   }
 
-  public async play(url: string) {
-    const stream = ytdl(url, {
-      quality: "highestaudio",
-      highWaterMark: 1 << 62,
-    });
-    stream.on("error", (err) => {
-      console.log(err);
-    });
-  }
+//   public async play(url: string) {
+//     const stream = ytdl(url, {
+//       quality: "highestaudio",
+//       highWaterMark: 1 << 62,
+//     });
+//     stream.on("error", (err) => {
+//       console.log(err);
+//     });
+//   }
 
-  private async search(query: string) {
-    const result = ytsr(query, { limit: 20 });
-    console.log(result);
-  }
+//   private async search(query: string) {
+//     const result = ytsr(query, { limit: 20 });
+//     console.log(result);
+//   }
 
-  public get getQueue(): string {
-    const str: string[] = [];
-    for (const q of this.queue) {
-    }
-    return str.join("");
-  }
+//   public get getQueue(): string {
+//     const str: string[] = [];
+//     for (const q of this.queue) {
+//     }
+//     return str.join("");
+//   }
 
-  public set setRepeatMode(mode: repeatMode) {
-    this.repeatMode = mode;
-  }
-}
+//   public set setRepeatMode(mode: repeatMode) {
+//     this.repeatMode = mode;
+//   }
+// }
 
-export default musicPlayer;
+// export default musicPlayer;
